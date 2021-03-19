906f7356c8dfb5d5294d76773f970f6a84ebd0aa
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-03-18 21:51:03 -0400

    Update ggplot-sandbox.R

--- ggplot-sandbox.R
+++ ggplot-sandbox.R
@@ -3,8 +3,16 @@ library(ggplot2)
 
 ggplot(data = data, aes(x, y, color = outcome, alpha=outcome, size=outcome)) + 
   geom_point() +
-  scale_color_manual(values = c("grey","orange","red")) +
-  scale_alpha_manual(values = c(0.1, 0.9, 1.0)) +
-  scale_size_manual(values = c(0.5, 0.5, 0.7)) +
+  scale_color_manual(values = c(brewer.pal(9, "Blues")[2],
+                                brewer.pal(9, "Purples")[8],
+                                brewer.pal(9, "Set1")[1])) +
+  scale_alpha_manual(values = c(0.1, 1.0, 1.0)) +
+  scale_size_manual(values = c(0.5, 0.6, 1)) +
   facet_grid(cols = vars(arm)) +
   theme_void()
+
+RColorBrewer::display.brewer.all(type = "qual")
+brewer.pal(9, "Set1")[1] # negative
+brewer.pal(9, "Blues")[5] 
+
+RColorBrewer::brewer.pal.info
