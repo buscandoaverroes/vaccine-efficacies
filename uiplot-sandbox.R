4f55c95ca8a4af93d101c260095f47525252cc47
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-03-29 16:44:32 -0400

    Update uiplot-sandbox.R

--- uiplot-sandbox.R
+++ uiplot-sandbox.R
@@ -28,12 +28,61 @@ data <- select(vax_data, short_name,
    values_to = "value")  %>%
   separate(indicator, into = c("arm", "indicator"), sep = '_', extra = 'merge') %>%
   mutate(arm = stringr::str_to_title(arm)) %>% # convert to title case
-  pivot_wider( id_cols = c("short_name", "arm"), names_from = "indicator", values_from = "value") %>%
-  filter(short_name == "Pfizer")
+  #pivot_wider( id_cols = c("short_name", "arm"), names_from = "indicator", values_from = "value") %>%
+  filter(short_name == "Pfizer", indicator == "covid_rate10k" | indicator == "severe_rate10k") #  
 
 
 # ggplot ====================
-ggplot(data, aes(arm, covid_rate_pct)) +
-  geom_col() 
+## v1: dodged, grouped by indicator -----
+gg <- data %>%
+  ggplot(., aes(indicator, value), ) +
+  geom_col(aes(fill = arm), position = 'dodge') +
+  scale_fill_viridis_d(
+    aesthetics = "fill",
+    option = "viridis",
+  ) +
+  scale_x_discrete(labels=c("Covid", "Severe Covid")) +
+  labs(y = "Rate per 10k", fill = NULL, x = NULL) +
+  theme_minimal() + 
+  theme(
+    axis.title.x = NULL,
+    axis.title.y = element_text(size=10, margin = margin(t=0,r=8,b=0,l=0), face = 'bold'),
+    axis.text.x = element_text(size=10, face = 'bold'),
+    axis.text.y = element_text(size=10),
+    legend.title = NULL,
+    legend.text = element_text(size=10),
+    legend.key.size = unit(4,"mm"),
+    panel.grid = element_blank(),
+  ) 
+gg  
 
+
+gg_p <- ggplotly(gg) # for hover...
+gg_p
 # works for covid rate, how to incorporate severe and mortality?
+
+
+## v2: stacked, grouped by arm
+gg2 <- data %>%
+  ggplot(., aes(arm, value)) +
+  geom_col(aes(fill = indicator), position = 'stack', width = 0.6) +
+  scale_fill_viridis_d(
+    aesthetics = "fill",
+    option = "viridis",
+    labels = c("Covid",
+               "Severe Covid")
+  ) +
+  scale_x_discrete(labels=c("Placebo", "Vaccine")) +
+  labs(y = "Rate per 10k", x = NULL, fill = NULL) +
+  theme_minimal() + 
+  theme(
+    axis.title.x = NULL,
+    axis.title.y = element_text(size=10, margin = margin(t=0,r=8,b=0,l=0), face = 'bold'),
+    axis.text.x = element_text(size=10, face = 'bold'),
+    axis.text.y = element_text(size=10),
+    legend.title = NULL,
+    legend.text = element_text(size=10),
+    legend.key.size = unit(4,"mm"),
+    panel.grid = element_blank(),
+  ) 
+gg2   
