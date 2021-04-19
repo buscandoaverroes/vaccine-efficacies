eeae914b55a32014bdd42f3adea622c4e1a0f776
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-04-19 15:41:41 -0400

    tabling bibliography issue for now

--- main.R
+++ main.R
@@ -3,6 +3,8 @@
 
 library(tidyverse)
 library(readr)
+library(rmarkdown)
+library(knitr)
 
 
 
@@ -27,5 +29,8 @@ if (user == 1) {
 s1  <- 1 # imports, calculates data
 s2  <- 1 # creates graph/app data
 
+m1  <- 1 # about markdown
+
 if (s1==1) {source(file.path(code, "clinical-data.R"))}
 if (s2==1) {source(file.path(code, "graph-data.R"))}
+#if (m1--1) (knit(input = file.path(app, "md/about.Rmd", output = file.path(app, "www"))))
