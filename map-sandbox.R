02ba69198040693dfc01ec9986570681c954a046
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-04-27 10:53:46 -0400

    Update map-sandbox.R

--- map-sandbox.R
+++ map-sandbox.R
@@ -3,6 +3,8 @@
 library(mapview)
 library(sf)
 library(tigris)
+library(leaflet)
+library(leafsync)
 
 load("/Volumes/PROJECTS/vaccines/data/infection-data.Rdata")
 
@@ -14,10 +16,15 @@ mapview(us_adm2_sf, zcol = "incidence_2wk_1000py"
 )
 
 # same scale, in new infections per 10k, most easily interpretable
-mapview(us_adm2_sf, zcol = "incidence_2wk_10k")
+m1 <- mapview(us_adm2_sf, zcol = "incidence_2wk_10k",
+              at = c(0, 10, 20, 50, 100, 500))
 
 
 # mapping protection chances
-mapview(us_adm2_sf, zcol = "protection_66", 
+m2 <- mapview(us_adm2_sf, zcol = "protection_66", 
         at = c(0, 0.95, 0.98, 0.99, 1)
-        )
+        ) 
+      # be able to toggle back and forth between protection variables, either in mv or shiny
+
+sync(m1, m2, 
+     ncol = 1)
