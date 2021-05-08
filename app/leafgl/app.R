698bde8284d9b2695f3c0e4b6c51c0b6812320bf
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-05-08 19:53:51 -0400

    Create app.R

---
+++ app/leafgl/app.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(leafgl)
+library(leaflet)
+
+
+
+# load data 
+load("data/app-data.Rdata")
+load("data/map-data.Rdata")
+
+
+ui <- fluidPage(
+
+   
+)
+
+server <- function(input, output) {
+
+}
+
+shinyApp(ui = ui, server = server)
